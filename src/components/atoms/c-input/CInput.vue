4cd19f245ca78fdece05e9cf168b17275ae71353
vinicius aragao <disanti2010@gmail.com>
2021-01-05 18:13:07 -0300

    feat: adding and saving customer identification step

---
+++ src/components/atoms/c-input/CInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input @input="handleInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CInput',
+
+  props: {
+    model: {
+      type: [String, Number]
+    },
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+
+  model: {
+    prop: 'model',
+    event: 'input'
+  },
+
+  methods: {
+    handleInput(model) {
+      this.$emit('input', model.target.value);
+    }
+  }
+};
+</script>
---
+++ src/views/orders/Orders.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Orders</h1>
+
+    <c-button @click="goToCustomerIdentification">new order</c-button>
+  </div>
+</template>
+
+<script>
+import CButton from '@/components/atoms/c-button/CButton';
+
+export default {
+  name: 'Home',
+
+  components: {
+    CButton
+  },
+
+  methods: {
+    goToCustomerIdentification() {
+      this.$router.push('/order/customer');
+    }
+  }
+};
+</script>
