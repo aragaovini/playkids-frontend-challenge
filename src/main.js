9b2d02559956aaee502dbf419787de218e4a7e58
vinicius aragao <disanti2010@gmail.com>
2021-01-06 13:28:25 -0300

    feat: adding vue currency to format currency values

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueCurrencyFilter from 'vue-currency-filter';
+
+Vue.use(VueCurrencyFilter, {
+  symbol: 'R$',
+  thousandsSeparator: '.',
+  fractionCount: 2,
+  fractionSeparator: ',',
+  symbolPosition: 'front',
+  symbolSpacing: true,
+  avoidEmptyDecimals: undefined
+});
 
 Vue.config.productionTip = false;
 
